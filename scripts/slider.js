481aa2cf4c3c5c6125b42dca54f5641e3bc73790
Millexs <david.lucas.santiago02@gmail.com>
2024-03-07 01:45:27 -0300

    Base do carrossel feita

---
+++ scripts/slider.js
@@ -0,0 +1,18 @@
+
+let radio = document.querySelector('.manual-btn')
+let cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(()=>{
+    proximaImg()
+},5000)
+
+const proximaImg = ()=>{
+    cont++
+    if(cont > 5){
+        cont = 1
+    }
+
+    document.getElementById('radio' + cont).checked = true
+}
